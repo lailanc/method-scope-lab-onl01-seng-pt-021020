bf4e4e6105c0b90a93cc7a2441a5256df7616f53
Laila Campbell <lailacampbell96@gmail.com>
2020-02-15 02:31:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 def catch_phrase(phrase="It's-a me, Mario!")
   puts phrase
    phrase = "It's-a me, Mario!"
+=======
+def catch_phrase(phrase="Its-a me Mario!")
+  puts "Its-a me Mario!" 
+  phrase = "Its-a me Mario!"
+>>>>>>> 98bf497b9b148467a69c3fa713cc58401cf4bb0a
 end
