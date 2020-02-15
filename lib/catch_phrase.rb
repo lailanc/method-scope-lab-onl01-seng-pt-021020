8a52b3b7b976a59fe40a0bdc6c7ac1b6e45422bc
Laila Campbell <lailacampbell96@gmail.com>
2020-02-15 02:32:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -8,3 +8,12 @@ def catch_phrase(phrase="Its-a me Mario!")
   phrase = "Its-a me Mario!"
 >>>>>>> 98bf497b9b148467a69c3fa713cc58401cf4bb0a
 end
+
+
+require "spec_helper"
+describe "#catch_phrase" do
+  it "puts out a catch phrase" do
+    phrase = "It's-a me, Mario!"
+    expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
+  end
+end
